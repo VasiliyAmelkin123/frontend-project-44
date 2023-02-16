e477acef80e1980e37f294ab57a3fb0951e8593e
Vasiliy Amelkin <w.amelkin@yandex.ru>
2023-02-16 13:56:59 +0300

    realize logic gcd-game

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,7 +1,45 @@
 import gameApi from '../game-api.js'
+import { findGcd, generateRandomNum } from '../utils.js'
 
 const gcdGame = () => {
-    console.log('Working...')
+    const winScore = 3
+    let resultScore = 0
+    let isGame = true
+
+    gameApi.logWelcomeGame()
+    const userName = gameApi.getUserName()
+    gameApi.logGrettingUserByName(userName)
+    gameApi.logGameRules('Find the greatest common divisor of given numbers.')
+
+    while(resultScore < winScore && isGame) {
+        const userQuestion = generateQuestion()
+        gameApi.logQuestion(`Question: ${userQuestion.question}`)
+        const userAnswer = gameApi.askQuestion('Your answer: ')
+
+        if (userAnswer == userQuestion.answer) {
+            gameApi.logCorrectAnswer()
+            resultScore += 1
+        } else {
+            gameApi.logFailedAnswer(userName, userQuestion.answer, userAnswer)
+            isGame = false
+        }
+    }
+
+    if (resultScore === winScore) {
+        gameApi.logWin(userName)
+    }
+
+    function generateQuestion() {
+        const num1 = generateRandomNum(10)
+        const num2 = generateRandomNum()
+        const question = `${num1} ${num2}`
+        const answer = findGcd(num1, num2)
+
+        return {
+            question,
+            answer
+        }
+    }
 }
 
 export default gcdGame
--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,11 @@ export const solveExample = (num1, num2, operation) => {
         case '*': return num1 * num2
     }
 }
+
+export const findGcd = (num1, num2) => {
+    if (!num2) {
+        return num1;
+      }
+
+      return findGcd(num2, num1 % num2);
+}
